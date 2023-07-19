11254eeb37dfbf51efce1d4a573d0b2e26038455
annasahebbansode1 <annasahebbansode1@gmail.com>
2023-07-19 00:26:02 -0700

    simple program
    simple program

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName);
+var firstNameAndLastName ="My First and Last Name = Annasaheb Bansode";
+console.log(firstNameAndLastName);
+var myFullName ="My Full name is = Annasaheb Dattatray Bansode";
+console.log(myFullName);
+var myLove;
+console.log(myLove);
+myLove="I Love My Js"
+console.log(myLove);
+var firstName ="My First Name Is = Annasaheb"
+console.log(firstName);
+var lastName ="My Last Name Is = Bansode"
+console.log(lastName);
+var age ="My Age Is =27"
+console.log(age);
+var myAddress ="My Address Is = Pune"
+console.log(myAddress);
+var myPincode ="My Pincode Is = 411041"
+console.log(myPincode);
+var myGender ="My Gender Is = Male"
+console.log(myGender);
+var married = "Are You Married = yes"
+console.log(married);
